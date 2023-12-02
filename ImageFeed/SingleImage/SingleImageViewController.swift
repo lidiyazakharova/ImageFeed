955a01b367a66172f0ae71bb920a1ba3e8575c2a
lidiyazakharova <lidijaromanowa@gmail.com>
2023-12-03 00:18:18 +0300

    Добавлен scroll и zoom

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -9,14 +9,25 @@ final class SingleImageViewController: UIViewController {
             }
         }
     
-    @IBOutlet var imageView: UIImageView!
+    @IBOutlet private var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
             super.viewDidLoad()
+        
             imageView.image = image
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
+        
         }
     
     @IBAction private func didTapBackButton() {
         dismiss(animated: true, completion: nil)
     }
 }
+
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        imageView
+    }
+} 
