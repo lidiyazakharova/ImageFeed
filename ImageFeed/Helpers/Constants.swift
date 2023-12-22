a33c94915a7d39813b8c7314a21d58c4a101039d
lidiyazakharova <lidijaromanowa@gmail.com>
2023-12-22 21:34:53 +0300

    добавлен authViewController

---
+++ ImageFeed/Helpers/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let AccessKey = "VxG9li5SL5IH3aD7qCDXVZQ7Klv-qAbvIduwAh_rJAc"
+let SecretKey = "fJLnw76SaLMIqOVzKKN5qyr8Ke-V8eCULogEnZsH5ik"
+let RedirectURI = "<ваш Redirect URI>"
+let AccessScope = "public+read_user+write_likes"
+let DefaultBaseURL = URL(string: "https://api.unsplash.com")!
