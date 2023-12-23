4761e8ae44090dabbdb01a67dc63b30bb9e3e891
lidiyazakharova <lidijaromanowa@gmail.com>
2023-12-24 00:40:24 +0300

    обновлен экран webview

--- ImageFeed/Auth/AuthViewController.swift
+++ ImageFeed/Auth/AuthViewController.swift
@@ -1,8 +1,11 @@
 import UIKit
 
 final class AuthViewController: UIViewController {
+    
+    private let ShowWebViewSegueIdentifier = "ShowWebView"
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return.lightContent
     }
+    
 }
