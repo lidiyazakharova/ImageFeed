d0da78ff54ba1e53d50b3b56dd33ec1cc06e5796
lidiyazakharova <lidijaromanowa@gmail.com>
2024-02-04 11:59:18 +0300

    рефакторинг web view

---
+++ ImageFeed/Auth/WebViewPresenter.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+public protocol WebViewPresenterProtocol {
+    var view: WebViewViewControllerProtocol? { get set }
+    func viewDidLoad()
+    func didUpdateProgressValue(_ newValue: Double)
+    func fetchCode(from url: URL) -> String?
+}
+
+final class WebViewPresenter: WebViewPresenterProtocol {
+    weak var view: WebViewViewControllerProtocol?
+    
+    func viewDidLoad() {
+        loadWebView()
+        didUpdateProgressValue(0)
+    }
+    
+    func didUpdateProgressValue(_ newValue: Double) {
+        let newProgressValue = Float(newValue)
+        view?.setProgressValue(newProgressValue)
+        
+        let shouldHideProgress = shouldHideProgress(for: newProgressValue)
+        view?.setProgressHidden(shouldHideProgress)
+    }
+    
+    func shouldHideProgress(for value: Float) -> Bool {
+        abs(value - 1.0) <= 0.0001
+    }
+    
+    
+    private func loadWebView(){
+        guard
+            var urlComponents = URLComponents(string: Constants.unsplashAuthorizeURL)
+        else { return }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: Constants.accessKey),
+            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
+            URLQueryItem(name: "response_type", value: "code"),
+            URLQueryItem(name: "scope", value: Constants.accessScope)
+        ]
+        guard
+            let url = urlComponents.url else { return }
+        let request = URLRequest(url: url)
+        view?.load(request: request)
+    }
+    
+    func fetchCode(from url: URL) -> String? {
+        if let urlComponents = URLComponents(string: url.absoluteString),
+           urlComponents.path == Constants.authorizedPath,
+           let items = urlComponents.queryItems,
+           let codeItem = items.first(where: { $0.name == "code" })
+        {
+            return codeItem.value
+        } else {
+            return nil
+        }
+    }
+} 
--- ImageFeed/Auth/WebViewViewController.swift
+++ ImageFeed/Auth/WebViewViewController.swift
@@ -6,8 +6,16 @@ protocol WebViewViewControllerDelegate: AnyObject {
     func webViewViewControllerDidCancel(_ vc: WebViewViewController)
 }
 
-final class WebViewViewController: UIViewController {
+public protocol WebViewViewControllerProtocol: AnyObject {
+    var presenter: WebViewPresenterProtocol? { get set }
+    func load(request: URLRequest)
+    func setProgressValue(_ newValue: Float)
+    func setProgressHidden(_ isHidden: Bool)
+}
+
+final class WebViewViewController: UIViewController & WebViewViewControllerProtocol {
     
+    var presenter: WebViewPresenterProtocol?
     weak var delegate: WebViewViewControllerDelegate?
     @IBOutlet private var webView: WKWebView!
     @IBOutlet private var progressView: UIProgressView!
@@ -19,26 +27,35 @@ final class WebViewViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        progressView.progress = 0.0
+//        progressView.progress = 0.0
         webView.navigationDelegate = self
-        loadWebView()
+        presenter?.viewDidLoad()
+        
         estimatedProgressObservation = webView.observe(
             \.estimatedProgress,
              options: [],
-             changeHandler: { [weak self] _, _ in
+             changeHandler: { [weak self] _,_ in
                  guard let self = self else { return }
-                 self.updateProgress()
+                 self.presenter?.didUpdateProgressValue(self.webView.estimatedProgress)
              })
+    }
+    
+    func load(request: URLRequest) {
+        webView.load(request)
+    }
+    
+    func setProgressValue(_ newValue: Float) {
+        progressView.progress = newValue
+    }
+    
+    func setProgressHidden(_ isHidden: Bool) {
+        progressView.isHidden = isHidden
     }
     
     @IBAction private func didTapBackButton(_ sender: Any) {
         delegate?.webViewViewControllerDidCancel(self)
     }
     
-    private func updateProgress() {
-        progressView.setProgress(Float(webView.estimatedProgress), animated: true)
-        progressView.isHidden = fabs(webView.estimatedProgress - 1.0) <= 0.0001
-    }
 }
 
 extension WebViewViewController: WKNavigationDelegate {
@@ -56,33 +73,46 @@ extension WebViewViewController: WKNavigationDelegate {
     }
     
     private func fetchCode(from navigationAction: WKNavigationAction) -> String? {
-        if
-            let url = navigationAction.request.url,
-            let urlComponents = URLComponents(string: url.absoluteString),
-            urlComponents.path == Constants.authorizedPath,
-            let items = urlComponents.queryItems,
-            let codeItem = items.first(where: { $0.name == "code" })
-        {
-            return codeItem.value
-        } else {
-            return nil
+        if let url = navigationAction.request.url {
+            return presenter?.fetchCode(from: url)
         }
+        return nil
     }
     
-    private func loadWebView(){
-        guard
-            var urlComponents = URLComponents(string: Constants.unsplashAuthorizeURL)
-        else { return }
-        
-        urlComponents.queryItems = [
-            URLQueryItem(name: "client_id", value: Constants.accessKey),
-            URLQueryItem(name: "redirect_uri", value: Constants.redirectURI),
-            URLQueryItem(name: "response_type", value: "code"),
-            URLQueryItem(name: "scope", value: Constants.accessScope)
-        ]
-        guard
-            let url = urlComponents.url else { return }
-        let request = URLRequest(url: url)
-        webView.load(request)
-    }
+    
 }
+
+
+//Добавьте уведомление презентера о том, что WebView обновил значение estimatedProgress. В методе observeValue(forKeyPath:of:change:context:) вьюконтроллера замените вызов updateProgress() на presenter?.didUpdateProgressValue(webView.estimatedProgress):
+//override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+//    if keyPath == #keyPath(WKWebView.estimatedProgress) {
+//        presenter?.didUpdateProgressValue(webView.estimatedProgress)
+//    } else {
+//        super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+//    }
+//}
+
+
+//override func viewDidAppear(_ animated: Bool) {
+//    super.viewDidAppear(animated)
+//    // NOTE: Since the class is marked as `final` we don't need to pass a context.
+//    // In case of inhertiance context must not be nil.
+//    webView.addObserver(
+//        self,
+//        forKeyPath: #keyPath(WKWebView.estimatedProgress),
+//        options: .new,
+//        context: nil)
+//}
+//
+//override func viewWillDisappear(_ animated: Bool) {
+//    super.viewWillDisappear(animated)
+//    webView.removeObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), context: nil)
+//}
+//
+//override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+//    if keyPath == #keyPath(WKWebView.estimatedProgress) {
+//        presenter?.didUpdateProgressValue(webView.estimatedProgress)
+//    } else {
+//        super.observeValue(forKeyPath: keyPath, of: object, change: change, context: context)
+//    }
+//}
